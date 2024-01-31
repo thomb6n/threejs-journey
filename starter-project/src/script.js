bfd3980e7f0b8bea27a88d6a4f65218fe9df54b1
Tom de Visser <tom.d6r@outlook.com>
2024-01-31 18:33:38 +0100

    Added initial mesh and GUI setting

--- starter-project/src/script.js
+++ starter-project/src/script.js
@@ -6,22 +6,23 @@ import "./style.css";
 const variables = {
   width: window.innerWidth,
   height: window.innerHeight,
+  materialColor: "#ffeded",
 };
-
-const gui = new GUI({
-  width: 200,
-  title: "Debug Panel",
-  closeFolders: true,
-});
 
 const canvas = document.querySelector("canvas.three");
 const scene = new THREE.Scene();
 
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: variables.materialColor })
+);
+scene.add(cube);
+
 const camera = new THREE.PerspectiveCamera(
-  65,
+  35,
   variables.width / variables.height
 );
-camera.position.z = 3;
+camera.position.z = 6;
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
@@ -29,23 +30,35 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(variables.width, variables.height);
 renderer.setClearColor(0x111111);
 
+window.addEventListener("resize", (e) => {
+  variables.height = e.target.window.innerHeight;
+  variables.width = e.target.window.innerWidth;
+
+  camera.aspect = variables.width / variables.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(variables.width, variables.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
 const clock = new THREE.Clock();
 
 const updateFrame = () => {
   const elapsedTime = clock.getElapsedTime();
+
   renderer.render(scene, camera);
+
   window.requestAnimationFrame(updateFrame);
 };
 
 updateFrame();
 
-window.addEventListener("resize", (e) => {
-  variables.height = e.target.window.innerHeight;
-  variables.width = e.target.window.innerWidth;
-  camera.aspect = variables.width / variables.height;
+const gui = new GUI({
+  width: 200,
+  title: "Debug Panel",
+  closeFolders: true,
+});
 
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(variables.width, variables.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+gui.addColor(variables, "materialColor").onChange(() => {
+  cube.material.color.set(variables.materialColor);
 });
